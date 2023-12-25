c6d44ce853700856397d4b6382153c17173ecbfa
Avinash Ghadge <144892125+Usernamecopieded@users.noreply.github.com>
2023-12-25 15:54:48 +0530

    Add files via upload
    This repository contains a Quiz Web App built using HTML, CSS, JavaScriptd and The app allows users to test their knowledge by answering quiz questions from various categories.
    
    Features
    User-friendly interface for a seamless quiz-taking experience.
    Randomly generated questions from the Open Trivia Database API.
    Multiple-choice questions with instant feedback on answer selection.
    Scoring system to track and display the user's progress.
    Different categories of quizzes to choose from.
    Technologies Used
    HTML: Used to structure the web pages and content.
    CSS: Used for styling and enhancing the visual appearance of the app.
    JavaScript: Implemented the logic and interactivity of the quiz app.

---
+++ questions.js
@@ -0,0 +1,143 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+
+//--------------------- Full Stack -------------------------------
+
+{
+  numb: 6,
+  question: "What does the term Full Stack Development refer to?",
+  answer: "Development that involves front-end and back-end programming",
+  options: [
+    "Development that involves stack data structures",
+    "Development that involves front-end and back-end programming",
+    "Development that involves only backend programming",
+    "None of above"
+    
+  ]
+},
+  {
+  numb: 7,
+  question: "Which of the following is not a front-end technology?",
+  answer: "SQL",
+  options: [
+    "HTML",
+    "Javascript",
+    "SQL",
+    "CSS"
+  ]
+},
+
+
+{
+  numb: 8,
+  question: "Amongst which of the following programming language is used as a server-side language?",
+  answer: "Both A and C",
+  options: [
+    "Python",
+    "C++",
+    "Javascript",
+    "Both A and C"
+  ]
+},
+
+  {
+  numb: 9,
+  question: "Database in Full stack development is used to ____.?",
+  answer: "Storing and retrieving data",
+  options: [
+    "Rendering web pages",
+    "Storing and retrieving data",
+    "Statement Question Language",
+    "Styling HTML pages"
+  ]
+},
+
+
+  {
+  numb: 10,
+  question: "What is Git?",
+  answer: "Version control system",
+  options: [
+    "Version control system",
+    "Framewok",
+    "package manager",
+    "Database"
+  ]
+ },
+
+
+
+ 
+// ================================= SAMPLE ====================================================
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
